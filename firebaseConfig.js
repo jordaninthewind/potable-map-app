3666402479535a9e3761a26d428a5fd5f2e439bc
jordaninthewind <jordan.kline@gmail.com>
2023-01-21 20:04:06 -0800

    remove unsupported analytics

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -20,6 +19,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 
-export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
