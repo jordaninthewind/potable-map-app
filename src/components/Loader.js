bf8161ae9b13c64365b8fb57cc3c2eb9a63fab70
jordaninthewind <jordan.kline@gmail.com>
2023-01-22 11:04:31 -0800

    fix loader layout

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Loader = ({ loading }) => {
+  const { bottom } = useSafeAreaInsets();
   return (
     <>
       {loading && (
@@ -16,14 +18,15 @@ const Loader = ({ loading }) => {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    bottom: 0,
     flex: 1,
     height: "100%",
-    width: "100%",
-    position: "absolute",
-    zIndex: 1,
-    backgroundColor: "rgba(255, 255, 255, 0.25)",
     justifyContent: "center",
-    alignItems: "center",
+    position: "absolute",
+    width: "100%",
+    zIndex: 1,
   },
 });
 
