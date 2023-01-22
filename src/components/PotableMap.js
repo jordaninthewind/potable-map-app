942deda1645b6b67776740306d7baa96eb4f5db2
jordaninthewind <jordan.kline@gmail.com>
2023-01-22 11:04:55 -0800

    add custom pin callout

--- src/components/PotableMap.js
+++ src/components/PotableMap.js
@@ -1,10 +1,15 @@
+import { useRef } from "react";
 import { useColorScheme, StyleSheet } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { Text } from "react-native-paper";
 
 const PotableMap = ({ location, markers, onMove, addPin, ...props }) => {
   const colorScheme = useColorScheme();
+  const mapRef = useRef(null);
+
   return (
     <MapView
+      ref={mapRef}
       onLongPress={addPin}
       onRegionChangeComplete={onMove}
       region={location}
@@ -18,7 +23,17 @@ const PotableMap = ({ location, markers, onMove, addPin, ...props }) => {
     >
       {markers?.map(({ location, title }, index) => {
         return (
-          <Marker key={`pin${index}`} coordinate={location} title={title} />
+          <Marker
+            key={`pin${index}`}
+            coordinate={location}
+            calloutVisible={true}
+          >
+            <Callout flat style={styles.callout}>
+              <Text>Title: {title}</Text>
+              <Text>Latitude: {location.latitude} </Text>
+              <Text>Longitude: {location.longitude} </Text>
+            </Callout>
+          </Marker>
         );
       })}
     </MapView>
@@ -26,6 +41,9 @@ const PotableMap = ({ location, markers, onMove, addPin, ...props }) => {
 };
 
 const styles = StyleSheet.create({
+  callout: {
+    padding: 30,
+  },
   map: {
     height: "100%",
     width: "100%",
