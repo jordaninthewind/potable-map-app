57b304918ad46699ab31b54f48ce1b58f066e346
jordaninthewind <jordan.kline@gmail.com>
2023-02-27 19:27:51 -0800

    add screen center, distance

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 import { GeoPoint } from "firebase/firestore";
+import { Dimensions } from "react-native";
 
 export const firebaseAdapter = ({ type }) => {
   switch (type) {
@@ -41,3 +42,45 @@ export const shortenString = (value, length) => {
 
   return value;
 };
+
+export const getDistance = (startLocation, endLocation) => {
+  const { latitude: lat1, longitude: lon1 } = startLocation;
+  const { latitude: lat2, longitude: lon2 } = endLocation;
+  const radius = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = radius * c; // Distance in km
+  return d;
+};
+
+export const deg2rad = (deg) => {
+  return deg * (Math.PI / 180);
+};
+
+export const getDistanceFromLatLonInKm = (startLocation, endLocation) => {
+  const { latitude: lat1, longitude: lon1 } = startLocation;
+  const { latitude: lat2, longitude: lon2 } = endLocation;
+
+  const radius = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = radius * c; // Distance in km
+
+  return distance;
+};
+
+export const centerMarkerInScreen = (latitude) => (latitude -= 0.0005);
