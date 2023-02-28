a4051f27d7371757b5019f8c006a0b9856f1de87
jordaninthewind <jordan.kline@gmail.com>
2023-02-27 21:28:52 -0800

    add shadow

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,14 @@
 export const DEFAULT_REGION = {
   latitude: 37.78825,
   longitude: -122.4324,
-  latitudeDelta: 0.8922,
-  longitudeDelta: 0.8421,
+  latitudeDelta: 0.15,
+  longitudeDelta: 0.15,
 };
 
 export const MARKER_DATABASE = "markers";
+
+export const PRIMARY_TEXT_SHADOW = {
+  textShadowColor: "rgba(255, 50, 255, 0.75)",
+  textShadowOffset: { width: 1, height: 2 },
+  textShadowRadius: 2,
+};
