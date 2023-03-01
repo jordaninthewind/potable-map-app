b46a447c7b6f66a6aac41a2bc52afd608b7ced02
jordaninthewind <jordan.kline@gmail.com>
2023-02-28 22:46:54 -0800

    lint

--- src/components/PotableApp.js
+++ src/components/PotableApp.js
@@ -1,9 +1,7 @@
 import Loader from "./Loader.js";
-import MenuGroup from "./MenuGroup.js";
 import ModalInterface from "./ModalInterface.js";
 import BottomNavigation from "./BottomNavigation.js";
 import NotificationOverlay from "./NotificationOverlay.js";
-import PotableMap from "./PotableMap.js";
 import PotableStatusBar from "./PotableStatusBar.js";
 
 const PotableApp = () => {
