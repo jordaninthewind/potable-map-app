164985da60896095341869ecd5e4fde3e450c30c
jordaninthewind <jordan.kline@gmail.com>
2023-02-28 23:26:49 -0800

    add state init

--- src/components/PotableApp.js
+++ src/components/PotableApp.js
@@ -3,8 +3,17 @@ import ModalInterface from "./ModalInterface.js";
 import BottomNavigation from "./BottomNavigation.js";
 import NotificationOverlay from "./NotificationOverlay.js";
 import PotableStatusBar from "./PotableStatusBar.js";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { initApp } from "../services/services.js";
 
 const PotableApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initApp());
+  }, []);
+
   return (
     <>
       <PotableStatusBar />
