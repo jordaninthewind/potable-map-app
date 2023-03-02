f030e59c7f592c30c20d1a35485056d00f4cf5b9
jordaninthewind <jordan.kline@gmail.com>
2023-03-01 21:00:35 -0800

    move user auth state to app

--- src/components/PotableApp.js
+++ src/components/PotableApp.js
@@ -6,12 +6,25 @@ import PotableStatusBar from "./PotableStatusBar.js";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { initApp } from "../services/services.js";
+import { getAuth } from "firebase/auth";
+import { setUser } from "../features/user/userSlice.js";
+import { setError } from "../features/error/errorSlice.js";
 
 const PotableApp = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(initApp());
+
+    getAuth().onAuthStateChanged((user) => {
+      console.log("onAuthStateChanged: ", user);
+      if (user) {
+        dispatch(setUser({ id: user.uid, email: user.email }));
+      } else {
+        dispatch(setUser(null));
+        dispatch(setError({ message: "User logged out" }));
+      }
+    });
   }, []);
 
   return (
