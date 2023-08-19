458c3bda782163079b1e73cf6c2933f6d26bd537
Jordan Kline <jordan.kline@gmail.com>
2023-08-19 12:29:29 -0700

    add module resolution

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,27 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@app": "./src",
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@constants": "./src/constants",
+            "@features": "./src/features",
+            "@navigation": "./src/navigation",
+            "@redux": "./src/redux",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@styles": "./src/styles",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
