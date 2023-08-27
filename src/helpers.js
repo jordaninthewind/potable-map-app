1ed6d555925f33aaa5cb1a49515e8221f7c9e6d1
Jordan Kline <jordan.kline@gmail.com>
2023-08-27 13:00:06 -0700

    add elipsis

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 export const shortenString = (value, length) => {
     if (value.length > length) {
-        return value.substring(0, length);
+        return value.substring(0, length) + '...';
     }
 
     return value;
